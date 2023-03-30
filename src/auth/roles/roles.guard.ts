54e42ab42d9023949424c3f74fdfaa86b8d0f9a4
Dima Serhiienko <dima.sergienko@gmail.com>
2023-03-30 09:52:40 +0300

    Update roles.guard: if (!token) return false. Delete redundant import from users.module

--- src/auth/roles/roles.guard.ts
+++ src/auth/roles/roles.guard.ts
@@ -9,16 +9,20 @@ export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-    if (!requiredRoles) {
-      return true;
+    try {
+      const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+        context.getHandler(),
+        context.getClass()
+      ]);
+      if (!requiredRoles) {
+        return true;
+      }
+
+      const user: any = jwt_decode(context.switchToHttp().getRequest().rawHeaders[1]);
+
+      return requiredRoles.includes(user.role) || user.id === +context.switchToHttp().getRequest().params.id;
+    } catch (e) {
+      return false
     }
-
-    const user: any = jwt_decode(context.switchToHttp().getRequest().rawHeaders[1])
-
-    return requiredRoles.includes(user.role) || user.id === +context.switchToHttp().getRequest().params.id
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module, ValidationPipe } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsersService } from "./users.service";
