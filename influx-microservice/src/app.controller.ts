1ffc414e0a54a17882e241f9a8400bcdd39af391
Dima Serhiienko <dima.sergienko@gmail.com>
2023-04-13 10:39:07 +0300

    Create function influx: write to influxdb test data

--- influx-microservice/src/app.controller.ts
+++ influx-microservice/src/app.controller.ts
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('influx')
+  async influxDB() {
+    await this.appService.influx();
+    return 'ok'
+  }
 }
--- influx-microservice/src/app.service.ts
+++ influx-microservice/src/app.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { InfluxDB, Point } from '@influxdata/influxdb-client'
+
+
+import { config } from 'dotenv';
+config();
+
+
+
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello Influx!';
+  }
+
+  async influx() {
+    const token = process.env.INFLUXDB_TOKEN
+    const url = 'http://localhost:8086'
+    const client = new InfluxDB({ url, token })
+
+    let org = `test-org`
+    let bucket = `test-bucket`
+
+    let writeClient = client.getWriteApi(org, bucket, 'ns')
+
+    for (let i = 0; i < 5; i++) {
+      let point = new Point('measurement1')
+        .tag('tagname1', 'tagvalue1')
+        .intField('field1', i)
+
+      await setTimeout(() => {
+        writeClient.writePoint(point)
+      }, i * 1000) // separate points by 1 second
+
+      await setTimeout(() => {
+        writeClient.flush()
+      }, 5000)
+    }
   }
 }
